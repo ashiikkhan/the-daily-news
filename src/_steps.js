dd01c7fd636e7d899ae5a4299b5913583f37f996
ashiikkhan <ashiksust06@gmail.com>
2022-10-22 02:04:51 +0600

    project set up

---
+++ src/_steps.js
@@ -0,0 +1,13 @@
+/**
+ * Project Set Up
+ * --------------
+ * 1. create a new react app [npx create-react-app the-daily-news]
+ * 2. create a new project on firebase [ give project name ]
+ * 3. register app [give project name ]
+ * 4. add firebase SDK [npm install firebase ]
+ * 5. initialize firebase by creating firebase.config.js file.
+ * 6. export app from firebase.config.js file.
+ * 7. react router dom [npm install react-router-dom]
+ * 8. install react bootstrap [npm install react-bootstrap bootstrap]
+ * 9. bootstrap.min.css in index.js/app.js file.
+ */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDA2_LWW2rXIHuJw_7z9YsFJ273pgCZm4I',
+  authDomain: 'the-daily-news-3a663.firebaseapp.com',
+  projectId: 'the-daily-news-3a663',
+  storageBucket: 'the-daily-news-3a663.appspot.com',
+  messagingSenderId: '947250572862',
+  appId: '1:947250572862:web:bec100ac43c09738802b6f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
